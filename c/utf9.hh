915d3925e9e4efd5466b8695c91478e2794ccdea
Spencer Tipping <spencer.tipping@gmail.com>
2022-06-21 19:44:49 -0600

    Defining comparison logic, solidifying the distinction between signed and unsigned (although it needs more work still)

--- c/utf9.hh
+++ c/utf9.hh
@@ -43,9 +43,8 @@ struct ibuf
       magic[1] = ibuf_ref_magic[1]; }
 
 
-  bool is_ibuf() const
-    { return magic[0] == ibuf_ref_magic[0] && magic[1] == ibuf_ref_magic[1]; }
-
+  bool is_ibuf() const { return magic[0] == ibuf_ref_magic[0]
+                             && magic[1] == ibuf_ref_magic[1]; }
 
   bool has  (uint64_t i) const { return i < l; }
   void check(uint64_t i) const { if (!has(i)) throw std::length_error("OOBE"); }
@@ -78,22 +77,51 @@ struct ibuf
                           return *(uint64_t*)(xs + i); }
 
 
-  uint8_t  c8 (uint64_t i) const { check(i);     return u8(i);  }
-  uint16_t c16(uint64_t i) const { check(i + 1); return u16(i); }
-  uint32_t c32(uint64_t i) const { check(i + 3); return u32(i); }
-  uint64_t c64(uint64_t i) const { check(i + 7); return u64(i); }
+  int8_t  i8 (uint64_t i) const { return reinterpret_cast<int8_t const*>(xs)[i]; }
+  int16_t i16(uint64_t i) const
+    { if constexpr (std::endian::native == std::endian::little)
+                     return __bswap_16(*(int16_t*)(xs + i));
+      else if constexpr (std::endian::native == std::endian::big)
+                          return *(int16_t*)(xs + i); }
+
+  int32_t i32(uint64_t i) const
+    { if constexpr (std::endian::native == std::endian::little)
+                     return __bswap_32(*(int32_t*)(xs + i));
+      else if constexpr (std::endian::native == std::endian::big)
+                          return *(int32_t*)(xs + i); }
+
+  int64_t i64(uint64_t i) const
+    { if constexpr (std::endian::native == std::endian::little)
+                     return __bswap_64(*(int64_t*)(xs + i));
+      else if constexpr (std::endian::native == std::endian::big)
+                          return *(int64_t*)(xs + i); }
+
+
+  uint8_t  ci8 (uint64_t i) const { check(i);     return i8(i);  }
+  uint16_t ci16(uint64_t i) const { check(i + 1); return i16(i); }
+  uint32_t ci32(uint64_t i) const { check(i + 3); return i32(i); }
+  uint64_t ci64(uint64_t i) const { check(i + 7); return i64(i); }
+
+  uint8_t  cu8 (uint64_t i) const { check(i);     return u8(i);  }
+  uint16_t cu16(uint64_t i) const { check(i + 1); return u16(i); }
+  uint32_t cu32(uint64_t i) const { check(i + 3); return u32(i); }
+  uint64_t cu64(uint64_t i) const { check(i + 7); return u64(i); }
+
+  float  f32(uint64_t i) const { let x = u32(i); return *(float *)(&x); }
+  double f64(uint64_t i) const { let x = u64(i); return *(double*)(&x); }
 };
 
 
-enum val_type
+enum val_type : uint8_t
 {
   REF     = 0,
-  INT     = 1,
-  FLOAT32 = 2,
-  FLOAT64 = 3,
-  SYMBOL  = 4,
-  PIDFD   = 5,
-  TAU     = 6,
+  UINT    = 1,
+  INT     = 2,
+  FLOAT32 = 3,
+  FLOAT64 = 4,
+  SYMBOL  = 5,
+  PIDFD   = 6,
+  TAU     = 7,
 
   ALPHA   = 8,
   OMEGA   = 9,
@@ -333,6 +361,14 @@ pfn const sfns[256] =
 }
 
 
+struct sym
+{
+  uint64_t h;
+
+  sym(std::string const &s) : h(std::hash<std::string>{}(s)) {}
+};
+
+
 struct val
 {
   union
@@ -341,33 +377,139 @@ struct val
 
   union
   { uint64_t i;
-    uint64_t vi;
+    uint64_t vu;
+    int64_t  vi;
+    sym      sy;
     double   vd;
     float    vf; };
 
 
   val(ibuf const &b_, uint64_t i_) : b(&b_),       i(i_)   {}
-  val(uint64_t vi_)                : tag(INT),     vi(vi_) {}
+  val(uint64_t vu_)                : tag(UINT),    vu(vu_) {}
+  val(int64_t vi_)                 : tag(INT),     vi(vi_) {}
   val(double vd_)                  : tag(FLOAT64), vd(vd_) {}
   val(float vf_)                   : tag(FLOAT32), vf(vf_) {}
+  val(sym sy_)                     : tag(SYMBOL),  sy(sy_) {}
 
 
   bool     has_ibuf() const { return tag >= 256 && b->is_ibuf(); }
-  val_type type()     const { return has_ibuf() ? bts[b->c8(i)] : static_cast<val_type>(tag); }
+  val_type type()     const { return has_ibuf() ? bts[b->cu8(i)] : static_cast<val_type>(tag); }
 
 
   val list() const
-    { uint64_t j = i; while (bts[b->c8(j)] == INDEX) j += b->len(j); return j; }
+    { uint64_t j = i; while (bts[b->cu8(j)] == INDEX) j += b->len(j); return j; }
 
-  uint8_t const *begin() const { return sfns[b->c8(i)](*b, i); }
+  uint8_t const *begin() const { return sfns[b->cu8(i)](*b, i); }
   uint8_t const *end()   const { return *b + b->len(i); }
+  val            first() const { return val(*b, begin() - b->xs); }
   val            next()  const { return val(*b, i + b->len(i)); }
 
+  uint64_t len() const
+    { if (!has_ibuf()) throw std::invalid_argument("len");
+      let x = b->u8(i);
+      if (x >= 0x48 && x <= 0x4f) return x - 0x48;
+      switch (x)
+      {
+      case 0x18: case 0x1c: return b->u8(i + 1);
+      case 0x19: case 0x1d: return b->u16(i + 1);
+      case 0x1a: case 0x1e: return b->u32(i + 1);
+      case 0x1b: case 0x1f: return b->u64(i + 1);
 
-  uint64_t hash()                const;
-  int      compare(val const &v) const;
-  val   operator[](val const &v) const;
-  val   operator[](uint64_t i)   const;
+      case 0x40: case 0x44: return b->u8(i + 2);
+      case 0x41: case 0x45: return b->u16(i + 2);
+      case 0x42: case 0x46: return b->u32(i + 2);
+      case 0x43: case 0x47: return b->u64(i + 2);
+
+      // TODO: define for indexes
+      } }
+
+
+  operator uint64_t() const
+    { if (type() != UINT) throw std::invalid_argument("uint64");
+      if (!has_ibuf())    return vu;
+      let x = b->u8(i);
+      switch (x)
+      {
+      case 0x00: return b->u8 (i + 1);
+      case 0x01: return b->u16(i + 1);
+      case 0x02: return b->u32(i + 1);
+      case 0x03: return b->u64(i + 1);
+      } }
+
+  operator int64_t() const
+    { if (type() != INT) throw std::invalid_argument("int64");
+      if (!has_ibuf())   return vi;
+      let x = b->u8(i);
+      if (x >= 0x80) return x - 0x80;
+      switch (x)
+      {
+      case 0x04: return b->i8 (i + 1);
+      case 0x05: return b->i16(i + 1);
+      case 0x06: return b->i32(i + 1);
+      case 0x07: return b->i64(i + 1);
+      } }
+
+  operator float() const
+    { if (type() != FLOAT32) throw std::invalid_argument("float32");
+      return has_ibuf() ? b->f32(i + 1) : vf; }
+
+  operator double() const
+    { if (type() != FLOAT64) throw std::invalid_argument("float64");
+      return has_ibuf() ? b->f64(i + 1) : vd; }
+
+
+  uint64_t hash() const;
+
+
+  int compare(val const &v) const
+    { let t1 = type();
+      let t2 = v.type();
+      if (t1 != t2) return (int) t1 - (int) t2;
+      switch (t1)
+      {
+
+#define cmpblock(t)                                     \
+        { let x1 = (t) *this;                           \
+          let x2 = (t) v;                               \
+          return x1 > x2 ? 1 : x1 < x2 ? -1 : 0; }
+
+      case UINT:    cmpblock(uint64_t)
+      case INT:     cmpblock(int64_t)
+      case FLOAT32: cmpblock(float)
+      case FLOAT64: cmpblock(double)
+
+#undef cmpblock
+
+      case UTF8:
+      case BYTES:
+      case ARRAY:
+      { let n1 =   end() -   begin();
+        let n2 = v.end() - v.begin();
+        let c  = std::__memcmp(begin(), v.begin(), std::min(n1, n2));
+        return c ? c : n1 > n2 ? 1 : n1 < n2 ? -1 : 0; }
+
+      case TUPLE:
+      { let      n1 =   len();
+        let      n2 = v.len();
+        uint64_t n  = std::min(n1, n2);
+        val      v1 =   first();
+        val      v2 = v.first();
+        while (n--)
+        { let c = v1.compare(v2);
+          v1 = v1.next();
+          v2 = v2.next();
+          if (c) return c; }
+        return n1 > n2 ? 1 : n1 < n2 ? -1 : 0; }
+
+      case INDEX: return list().compare(v.list());
+
+      default: throw std::invalid_argument("cmp");
+      }
+    }
+
+
+  val operator[](val const &v) const;
+  val operator[](uint64_t i)   const;
 };
 
 
@@ -397,29 +539,29 @@ let l17 = lf(17);
 
 
 // Value-length functions
-let str8_lf   = lf(2 + b.c8 (i + 1));
-let str16_lf  = lf(3 + b.c16(i + 1));
-let str32_lf  = lf(5 + b.c32(i + 1));
-let str64_lf  = lf(9 + b.c64(i + 1));
+let str8_lf   = lf(2 + b.cu8 (i + 1));
+let str16_lf  = lf(3 + b.cu16(i + 1));
+let str32_lf  = lf(5 + b.cu32(i + 1));
+let str64_lf  = lf(9 + b.cu64(i + 1));
 
-let list8_lf  = lf(3  + b.c8 (i + 1));
-let list16_lf = lf(5  + b.c16(i + 1));
-let list32_lf = lf(9  + b.c32(i + 1));
-let list64_lf = lf(17 + b.c64(i + 1));
+let list8_lf  = lf(3  + b.cu8 (i + 1));
+let list16_lf = lf(5  + b.cu16(i + 1));
+let list32_lf = lf(9  + b.cu32(i + 1));
+let list64_lf = lf(17 + b.cu64(i + 1));
 
-let u8_tvlf  = lf(b.c8 (i + 1));
-let u16_tvlf = lf(b.c16(i + 1));
-let u32_tvlf = lf(b.c32(i + 1));
-let u64_tvlf = lf(b.c64(i + 1));
+let u8_tvlf  = lf(b.cu8 (i + 1));
+let u16_tvlf = lf(b.cu16(i + 1));
+let u32_tvlf = lf(b.cu32(i + 1));
+let u64_tvlf = lf(b.cu64(i + 1));
 
-let fixutf8_lf   = lf(1 + (b.c8(i) - 0x20));
-let fixbytes_lf  = lf(1 + (b.c8(i) - 0x30));
-let fixtuple8_lf = lf(1 + b.c8(i + 1));
+let fixutf8_lf   = lf(1 + (b.cu8(i) - 0x20));
+let fixbytes_lf  = lf(1 + (b.cu8(i) - 0x30));
+let fixtuple8_lf = lf(1 + b.cu8(i + 1));
 let fixint_lf    = l1;
 
-let idx16_lf = lf(1 + 2 + b.c16(i + 1));
-let idx32_lf = lf(1 + 4 + b.c32(i + 1));
-let idx64_lf = lf(1 + 8 + b.c64(i + 1));
+let idx16_lf = lf(1 + 2 + b.cu16(i + 1));
+let idx32_lf = lf(1 + 4 + b.cu32(i + 1));
+let idx64_lf = lf(1 + 8 + b.cu64(i + 1));
 
 let bogus_lf =
   [](ibuf const &b, uint64_t i) -> uint64_t
@@ -434,12 +576,12 @@ inline uint64_t tuple_tl(ibuf const &b, uint64_t i, uint64_t n)
   return l;
 }
 
-let fixtuple_tlf = lf(2 + tuple_tl(b, i + 2, b.c8(i) - 0x48));
+let fixtuple_tlf = lf(2 + tuple_tl(b, i + 2, b.cu8(i) - 0x48));
 
 
 // Typecode value length functions
-let fixutf8_tvlf   = lf(b.c8(i) - 0x20);
-let fixbytes_tvlf  = lf(b.c8(i) - 0x30);
+let fixutf8_tvlf   = lf(b.cu8(i) - 0x20);
+let fixbytes_tvlf  = lf(b.cu8(i) - 0x30);
 let fixtuple8_tvlf = u8_tvlf;
 
 
@@ -478,9 +620,9 @@ lfn const lfns[256] =   // 2kiB dcache footprint (common case)
   fixtuple8_lf, fixtuple8_lf, fixtuple8_lf, fixtuple8_lf,
 
   // 0x50-0x5f
-  lf(1 + 2 + 1 + b.c16(i + 1)),
-  lf(1 + 4 + 1 + b.c32(i + 1)),
-  lf(1 + 8 + 1 + b.c64(i + 1)),
+  lf(1 + 2 + 1 + b.cu16(i + 1)),
+  lf(1 + 4 + 1 + b.cu32(i + 1)),
+  lf(1 + 8 + 1 + b.cu64(i + 1)),
   l1,
 
   idx16_lf, idx16_lf, idx16_lf, idx16_lf,
@@ -569,10 +711,10 @@ lfn const tlfns[256] =  // 640B dcache footprint (worst-case)
   l1, l1, l1, l1,
 
   // 0x40-0x4f
-  lf(3  + tuple_tl(b, i + 3,  b.c8 (i + 2))),
-  lf(5  + tuple_tl(b, i + 5,  b.c16(i + 3))),
-  lf(9  + tuple_tl(b, i + 9,  b.c32(i + 5))),
-  lf(17 + tuple_tl(b, i + 17, b.c64(i + 9))),
+  lf(3  + tuple_tl(b, i + 3,  b.cu8 (i + 2))),
+  lf(5  + tuple_tl(b, i + 5,  b.cu16(i + 3))),
+  lf(9  + tuple_tl(b, i + 9,  b.cu32(i + 5))),
+  lf(17 + tuple_tl(b, i + 17, b.cu64(i + 9))),
 
   lf(3  + b.tlen(i + 3)),
   lf(5  + b.tlen(i + 5)),
