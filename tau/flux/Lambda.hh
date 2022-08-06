93eee0b3e3286def06f78b53b023ae9f56ef770f
Spencer Tipping <spencer.tipping@gmail.com>
2022-08-06 11:11:16 -0600

    More lambda mechanics

--- tau/flux/Lambda.hh
+++ tau/flux/Lambda.hh
@@ -36,14 +36,25 @@ struct Λ             // manager of λs
   M<λi, Λλ> ls;      // all λs
   Q<λi>     rq;      // run schedule
   λi        ni{1};   // next λi
+  λi        ri{0};   // currently running λi
 
   λi   c(λf f)       { let   i = ni++;     ls.emplace(i, Λλ{λ<Λr>(f), λS}); r(i);   return i; }
   bool e(λi i) const {                                                              return ls.contains(i); }
   uN   n()     const {                                                              return ls.size(); }
   Λ   &r(λi i)       { auto &l = ls.at(i); assert(l.s != λR); rq.push(i); l.s = λR; return *this; }
+  Λ   &s(λi i)       { auto &l = ls.at(i); assert(l.s != λS);             l.s = λS; return *this; }
   Λ   &x(λi i)       {                     assert(e(i));      ls.erase(i);          return *this; }
 
 
+  bool operator()()
+    { assert(!ri);
+
+      // TODO: skip non-runnable tasks
+      if (rq.empty()) return false;
+      ri = rq.front(); rq.pop();
+      ls.at(ri).l();
+      ri = 0;
+      return true; }
 };
 
 
