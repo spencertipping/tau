666adb03610528c73d2f13d1728533652fd2daab
Spencer Tipping <spencer.tipping@gmail.com>
2022-09-12 09:12:37 -0600

    WIP xcb+gl+pango frames

--- tau/xi.hh
+++ tau/xi.hh
@@ -2,12 +2,20 @@
 #define τξ_h
 
 
+#include "arch.hh"
+
+
 #include "xi/basics.hh"
 #include "xi/theta.hh"
 #include "xi/digamma.hh"
 #include "xi/debug.hh"
 #include "xi/io.hh"
 #include "xi/tcp.hh"
+
+
+#if τarch == τarch_linux
+# include "linux/xframe.hh"
+#endif
 
 
 #endif
---
+++ tau/xi/linux/xframe.hh
@@ -0,0 +1,30 @@
+#ifndef τξlinux_xframe_h
+#define τξlinux_xframe_h
+
+
+#include <X11/Xlib.h>
+#include <X11/Xlib-xcb.h>
+
+#include <GL/gl.h>
+#include <GL/glx.h>
+#include <xcb/xcb.h>
+#include <xcb/xproto.h>
+
+#include <pango/pangocairo.h>
+
+
+#include "../begin.hh"
+
+namespace τ::ξ
+{
+
+
+// NOTE: https://stackoverflow.com/questions/18689863/obtain-keyboard-layout-and-keysyms-with-xcb
+// less good: https://stackoverflow.com/questions/34582279/linux-c-keymapping-keycodes
+
+}
+
+#include "../end.hh"
+
+
+#endif
