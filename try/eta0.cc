3a52885c64d7faa6daf8ea163d06a262e559fdae
Spencer Tipping <spencer.tipping@gmail.com>
2023-01-24 09:30:34 -0700

    More aggressive testing; it all works

--- try/eta0.cc
+++ try/eta0.cc
@@ -6,50 +6,60 @@
 using namespace τ;
 using namespace std;
 
-void try_ints()
+
+void test_uint(u64 i)
 {
   u8 buf[64] = {0};
+  η0o o = i; o.into(buf, sizeof(buf));
+  A(η0bc(buf, sizeof(buf)), "η₀bc failed on " << i);
+  η0i r{buf};
+  A(r.type() == η0t::uint_be,
+    r.type() << " ≠ " << η0t::uint_be << "; r = " << r);
+  let d = r.data();
+  uN  x = 0;
+  for (uN j = 0; j < r.size(); ++j) x = x << 8 | d[j];
+  A(x == i,
+    "uint_be decode fail: " << x << " ≠ " << i << "; r = " << r
+    << "; buf = " << Sc<uN>(buf[0])
+    << " " << Sc<uN>(buf[1])
+    << " " << Sc<uN>(buf[2])
+    << " " << Sc<uN>(buf[3])
+    << " " << Sc<uN>(buf[4])
+    << " " << Sc<uN>(buf[5])
+    << " " << Sc<uN>(buf[6]));
+}
+
+
+void test_int(i64 i)
+{
+  u8 buf[64] = {0};
+  η0o o = i; o.into(buf, sizeof(buf));
+  A(η0bc(buf, sizeof(buf)), "η₀bc failed on " << i);
+  η0i r{buf};
+  A(r.type() == η0t::int_be,
+    r.type() << " ≠ " << η0t::uint_be << "; r = " << r);
+  let d = r.data();
+  iN  x = *d & 128 ? -1 : 0;
+  for (uN j = 0; j < r.size(); ++j) x = x << 8 | d[j];
+  A(x == i,
+    "int_be decode fail: " << x << " ≠ " << i << "; r = " << r
+    << "; buf = " << Sc<uN>(buf[0])
+    << " " << Sc<uN>(buf[1])
+    << " " << Sc<uN>(buf[2])
+    << " " << Sc<uN>(buf[3])
+    << " " << Sc<uN>(buf[4])
+    << " " << Sc<uN>(buf[5])
+    << " " << Sc<uN>(buf[6]));
+}
+
+
+void try_ints()
+{
   for (uN i = 0; i < 1048576; ++i)
-  {
-    η0o o = i; o.into(buf, sizeof(buf));
-    A(η0bc(buf, sizeof(buf)), "η₀bc failed on " << i);
-    η0i r{buf};
-    A(r.type() == η0t::uint_be,
-      r.type() << " ≠ " << η0t::uint_be << "; r = " << r);
-    let d = r.data();
-    uN  x = 0;
-    for (uN j = 0; j < r.size(); ++j) x = x << 8 | d[j];
-    A(x == i,
-      "uint_be decode fail: " << x << " ≠ " << i << "; r = " << r
-      << "; buf = " << Sc<uN>(buf[0])
-             << " " << Sc<uN>(buf[1])
-             << " " << Sc<uN>(buf[2])
-             << " " << Sc<uN>(buf[3])
-             << " " << Sc<uN>(buf[4])
-             << " " << Sc<uN>(buf[5])
-             << " " << Sc<uN>(buf[6]));
-  }
+    test_uint(i), test_uint(i << 22), test_uint(i << 44);
 
   for (iN i = -1048576; i < 1048576; ++i)
-  {
-    η0o o = i; o.into(buf, sizeof(buf));
-    A(η0bc(buf, sizeof(buf)), "η₀bc failed on " << i);
-    η0i r{buf};
-    A(r.type() == η0t::int_be,
-      r.type() << " ≠ " << η0t::uint_be << "; r = " << r);
-    let d = r.data();
-    iN  x = *d & 128 ? -1 : 0;
-    for (uN j = 0; j < r.size(); ++j) x = x << 8 | d[j];
-    A(x == i,
-      "int_be decode fail: " << x << " ≠ " << i << "; r = " << r
-      << "; buf = " << Sc<uN>(buf[0])
-             << " " << Sc<uN>(buf[1])
-             << " " << Sc<uN>(buf[2])
-             << " " << Sc<uN>(buf[3])
-             << " " << Sc<uN>(buf[4])
-             << " " << Sc<uN>(buf[5])
-             << " " << Sc<uN>(buf[6]));
-  }
+    test_int(i), test_int(i << 22), test_int(i << 44);
 }
 
 
