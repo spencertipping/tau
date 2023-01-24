93a5e0cd70c8eb69f957ef6f73b4cfc30d7cfb53
Spencer Tipping <spencer.tipping@gmail.com>
2023-01-24 08:51:03 -0700

    Fixed up .gitignore, added new test file

---
+++ try/eta0.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "../tau.hh"
+#include "../tau/begin.hh"
+
+
+using namespace τ;
+using namespace std;
+
+void try_ints()
+{
+  u8 buf[64];
+  for (uN i = 0; i < 1048576; ++i)
+  {
+    η0o o = i; o.into(buf, sizeof(buf));
+    A(η0bc(buf, sizeof(buf)), "η₀bc failed on " << i);
+    η0i r{buf};
+    A(r.type() == η0t::uint_be,
+      r.type() << " ≠ " << η0t::uint_be << "; r = " << r);
+    let d = r.data();
+    uN  x = 0;
+    for (uN j = 0; j < r.size(); ++j) x = x << 8 | d[j];
+    A(x == i,
+      "uint_be decode fail: " << x << " ≠ " << i << "; r = " << r
+      << "; buf = " << Sc<uN>(buf[0])
+             << " " << Sc<uN>(buf[1])
+             << " " << Sc<uN>(buf[2]));
+  }
+}
+
+
+int main()
+{
+  τassert_begin;
+  try_ints();
+  return 0;
+  τassert_end;
+}
+
+
+#include "../tau/end.hh"
