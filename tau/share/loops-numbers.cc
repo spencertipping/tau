183f1eaada775736b5506b7e024acbdeb219a739
Spencer Tipping <spencer.tipping@gmail.com>
2023-02-05 09:54:23 -0700

    Simplifying some loops logic

--- tau/share/loops-numbers.cc
+++ tau/share/loops-numbers.cc
@@ -9,8 +9,7 @@ namespace τ
 Sp<γ> γι(u64 u)
 { return γffn("ι", [u](Sp<ψ>, ξi I, ξo o)
   { I.close();
-    for (u64 i = 0; i < u && o << η0o(i); ++i);
-    o.close(); }); }
+    for (u64 i = 0; i < u && o << η0o(i); ++i); }); }
 
 
 Sp<γ> γϊ()
@@ -19,24 +18,21 @@ Sp<γ> γϊ()
       if (η0ts{η0t::uint_be, η0t::int_be}[x.type()])
       { let n = Sc<i64>(η1pi{x});
         for (i64 j = 0; j < n; ++j) if (!(o << η0o{j})) goto done;
-        o << η0o(η0sig::τ); }
+        if (!(o << η0o(η0sig::τ))) goto done; }
       else
         if (!(o << x)) goto done;
-  done:
-    o.close(); }); }
+  done:; }); }
 
 
 Sp<γ> γtake(u64 n)
 { return γffn("↑", [=](Sp<ψ> q, ξi i, ξo o) mutable
-  { for (let x : i) if (!n-- || !(o << x)) break;
-    o.close(); }); }
+  { for (let x : i) if (!n-- || !(o << x)) break; }); }
 
 
 Sp<γ> γdrop(u64 n)
 { return γffn("↓", [=](Sp<ψ> q, ξi i, ξo o) mutable
   { for (let x : i) if (!n--) break;
-    for (let x : i) if (!(o << x)) break;
-    o.close(); }); }
+    for (let x : i) if (!(o << x)) break; }); }
 
 
 }
