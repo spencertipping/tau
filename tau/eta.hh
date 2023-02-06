11cbf3589a7ad9e8b5b31495811d007feaa0c01e
Spencer Tipping <spencer.tipping@gmail.com>
2023-02-05 17:21:19 -0700

    WIP scaffolding new eta API structs

--- tau/eta.hh
+++ tau/eta.hh
@@ -7,5 +7,29 @@
 #include "eta0o.hh"
 #include "eta1i.hh"
 
+#include "begin.hh"
+
+namespace τ
+{
+
+
+// Multi-level η input: combines functions from η₀ and η₁
+struct ηi
+{
+
+};
+
+
+// Multi-level η output: combines functions from η₀ and η₁
+struct ηo
+{
+
+};
+
+
+}
+
+#include "end.hh"
+
 
 #endif
