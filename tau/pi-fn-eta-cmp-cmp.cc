ba3c7b7a7504460d9f94db2dec1f13b7303185f9
Spencer Tipping <spencer.tipping@gmail.com>
2023-04-12 19:22:59 -0600

    Fixed wasm build, removed profiling

--- tau/pi-fn-eta-cmp-cmp.cc
+++ tau/pi-fn-eta-cmp-cmp.cc
@@ -5,6 +5,8 @@ namespace τ
 {
 
 
+τclang_workaround_gensyms(cmp)
+
 πdf πηcmp()  { return [](πi&, πv &&a, πv &&b) { return πv{cmp(a <=> b)}; }; }
 πdf πηvcmp() { return πdv(fn {τbinfallthrough(<=>), τvbinop(op_cmp)}); }
 
