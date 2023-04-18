1d25d938469f702471f1a95d12304e4c9ce14846
Spencer Tipping <spencer.tipping@gmail.com>
2023-04-18 17:43:19 -0600

    WIP new sigma psi4 stuff for HTTP

---
+++ sigma/psi4.hh
@@ -0,0 +1,21 @@
+#ifndef σΨ4_h
+#define σΨ4_h
+
+
+#include "begin.hh"
+
+namespace σ
+{
+
+
+// TODO: derive π grammars for function args
+τ::Ψ4 Ψhttp();
+τ::Ψ4 Ψauth();
+
+
+}
+
+#include "end.hh"
+
+
+#endif
