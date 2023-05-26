3b857c8bc2fe122a96975ab75f686fd78db92dd1
Spencer Tipping <spencer.tipping@gmail.com>
2023-05-26 16:44:07 -0600

    More pi-phi design: we must first build the parser structure, then .def() each function with pre-auto functions in order for type uniformity to work out

--- tau/pi-phi.hh
+++ tau/pi-phi.hh
@@ -12,25 +12,6 @@ namespace τ
 {
 
 
-// Core language, for testing/example
-// TODO: port these to the new parser structure
-
-φ<πf>  πφcore_a();
-φ<πmf> πφcore_m();
-φ<πdf> πφcore_d();
-φ<πtf> πφcore_t();
-
-φ<πmsf> πφcore_ms();
-φ<πdsf> πφcore_ds();
-φ<πtsf> πφcore_ts();
-
-φ<πsmf>  πφcore_sm();
-φ<πsmsf> πφcore_sms();
-
-
-φ<πfs> πφfs(φ<πfs> f);
-
-
 // π grammar generated from core parsers, written here in function
 // notation:
 //
@@ -53,8 +34,15 @@ namespace τ
 // TODO: this struct should be generated from a prefix dict so we can
 // more easily extend the grammar
 
-// TODO: πφ can use templates to implement stateful φauto for its constituent
-// functions; this way we sidestep the global-parser problem
+// TODO: πφ can use templates to implement stateful φauto for its
+// constituent functions; this way we sidestep the global-parser problem
+
+// TODO: write an example π operator function and define our expectations
+// about how its arguments would be auto-filled
+//
+// NOTE: we need one-off .def("prefix", []() {}) calls in order to
+// accommodate pre-auto parse template functions; some of the args to these
+// functions will be πf or πfs, which will be filled by s and p from πφ.
 
 struct πφ final
 {
@@ -74,17 +62,22 @@ struct πφ final
 };
 
 
+// Take a single plural-element context and parse multiple of them,
+// splicing the results into a single output list
+φ<πfs> πφfs(φ<πfs>);
+
+
 // Built-in language elements
-φ<St> πφword();
-φ<St> πφws();
-φ<St> πφlc();
-φ<St> πφig();
+φ<St> πφword();  // an ident-like bareword
+φ<St> πφws();    // whitespace
+φ<St> πφlc();    // line comment
+φ<St> πφig();    // ignored things (whitespace + line comments)
 
-φ<St> πφlb();
-φ<St> πφrb();
+φ<St> πφlb();    // wrapped left bracket
+φ<St> πφrb();    // wrapped right bracket
 
-φ<St> πφlp();
-φ<St> πφrp();
+φ<St> πφlp();    // wrapped left paren
+φ<St> πφrp();    // wrapped right paren
 
 
 Tt φ<T> πφwrap (φ<T> p) { return φ2("wrap", πφig(), p, πφig()); }
