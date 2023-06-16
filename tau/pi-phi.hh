109e1cfc335397a38991a21580a02a0cb9ac8183
Spencer Tipping <spencer.tipping@gmail.com>
2023-06-16 08:13:37 -0600

    Split parser functions into their own structs, which is necessary for full grammar extensibility

--- tau/pi-phi.hh
+++ tau/pi-phi.hh
@@ -15,47 +15,61 @@ namespace τ
 {
 
 
-// An extensible π grammar. See pi-phi.md for details.
-struct πφ final
+struct πφP
 {
-  πφ() : s_  (φa0<πf<1>>("πs")),
-         p_  (φa0<πf<1>>("πp")),
-         ssp_(φd <πf<0>>("πssp")),
-         psp_(φd <πf<0>>("πpsp")),
-         spp_(φd <πf<0>>("πspp")),
-         ppp_(φd <πf<0>>("πppp"))
-    {
-      // TODO: assemble toplevel parsers here
-    }
-
-
-  πφ &def_sa(φ<πf<1>> p) { s_.as<φa_<πf<1>>>() << p; return *this; }
-  πφ &def_pa(φ<πf<1>> p) { p_.as<φa_<πf<1>>>() << p; return *this; }
-
-  Tt πφ &def_ssp(St n, T f) { return def_(ssp_, n, f); }
-  Tt πφ &def_psp(St n, T f) { return def_(psp_, n, f); }
-  Tt πφ &def_spp(St n, T f) { return def_(spp_, n, f); }
-  Tt πφ &def_ppp(St n, T f) { return def_(ppp_, n, f); }
-  Tt πφ &def_sp (St n, T f) { return def_(sp_,  n, f); }
-  Tt πφ &def_pp (St n, T f) { return def_(pp_,  n, f); }
-
-
   Tt auto p(πP<T>&&) const
     { return φm(p(std::declval<T>()), [](T &&x) { return πP<T>{x}; }); }
+};
 
+struct πφlit
+{
   auto p(i64&&)   const { return πφint(); }
   auto p(f64&&)   const { return πφfloat(); }
   auto p(St&&)    const { return πφstr(); }
   auto p(ηname&&) const { return πφname(); }
+};
 
+struct πφstr
+{
   template<chc *S> auto p(φaL<S>&&) const { return φl(St{S}); }
   template<chc *S> auto p(φaO<S>&&) const { return φo(φl(St{S})); }
   template<chc *S, bool N, u32 L, u32 U> auto p(φaCs<S, N, L, U>&&) const
     { return φcs(S, N, L, U); }
+};
+
+
+// An extensible π grammar. See pi-phi.md for details.
+//
+// To use this struct, inherit from it and structs like the above to add
+// parsers.
+struct πφ_
+{
+  πφ_() : s_  (φa0<πf<1>>("πs")),
+          p_  (φa0<πf<1>>("πp")),
+          ssp_(φd <πf<0>>("πssp")),
+          psp_(φd <πf<0>>("πpsp")),
+          spp_(φd <πf<0>>("πspp")),
+          ppp_(φd <πf<0>>("πppp"))
+    {
+      // TODO: assemble toplevel parsers here
+    }
+
+  virtual ~πφ_() {}
+
+
+  πφ_ &def_sa(φ<πf<1>> p) { s_.as<φa_<πf<1>>>() << p; return *this; }
+  πφ_ &def_pa(φ<πf<1>> p) { p_.as<φa_<πf<1>>>() << p; return *this; }
+
+  Tt πφ_ &def_ssp(St n, T f) { return def_(ssp_, n, f); }
+  Tt πφ_ &def_psp(St n, T f) { return def_(psp_, n, f); }
+  Tt πφ_ &def_spp(St n, T f) { return def_(spp_, n, f); }
+  Tt πφ_ &def_ppp(St n, T f) { return def_(ppp_, n, f); }
+  Tt πφ_ &def_sp (St n, T f) { return def_(sp_,  n, f); }
+  Tt πφ_ &def_pp (St n, T f) { return def_(pp_,  n, f); }
 
 
 protected:
-  Tt πφ &def_(φ<πf<0>> &d, St n, T f)
+  Tt πφ_ &def_(φ<πf<0>> &d, St n, T f)
     { let p = φauto(*this, πPsplit(n, std::function(f)));
       d.as<φd_<πf<0>>>().def(n, p);
       return *this; }
@@ -72,6 +86,12 @@ protected:
   φ<πf<0>> sp_;   // singular → singular postfix (dispatch)
   φ<πf<0>> pp_;   // plural → plural postfix (dispatch)
 };
+
+
+struct πφ : public πφ_,
+            public πφP,
+            public πφlit,
+            public πφstr {};
 
 
 }
