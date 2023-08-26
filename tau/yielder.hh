542353a0225b107ef04e7af6873f36b50a3d58eb
Spencer Tipping <spencer.tipping@gmail.com>
2023-08-26 12:34:58 -0600

    Template args for yielder

--- tau/yielder.hh
+++ tau/yielder.hh
@@ -14,21 +14,21 @@ namespace τ
 // Λ yielder: nop that yields after a deadline since the last yield, optionally
 // calling a function instead
 
-struct λY final
+Txs struct λY final
 {
-  λY(ΔΘ dt = 10ms, F<void()> f = nullptr) : y_(never()), dt_(dt), f_(f) {}
+  λY(ΔΘ dt = 10ms, F<void(Xs...)> f = nullptr) : y_(never()), dt_(dt), f_(f) {}
 
-  void operator()()
+  void operator()(Xs &&... xs)
     { if (y_ == never()) y_ = now();
       if (now() - y_ < dt_) return;
-      if (f_) f_();
+      if (f_) f_(xs...);
       else    Λ_().y(λs::Y);
       y_ = now(); }
 
 protected:
-  Θp        y_;   // last yield
-  ΔΘ        dt_;  // deadline
-  F<void()> f_;
+  Θp             y_;   // last yield
+  ΔΘ             dt_;  // deadline
+  F<void(Xs...)> f_;
 };
 
 
