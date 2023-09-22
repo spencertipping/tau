f01a1442f0331942ce7c922f44241ccc98d23da5
Spencer Tipping <spencer.tipping@gmail.com>
2023-09-22 12:24:35 -0600

    Something weird is going on ... trying this again

--- sigma/server/pre-kv.cc
+++ sigma/server/pre-kv.cc
@@ -18,8 +18,8 @@ struct kv_lmdb_ final : public virtual kv_
   ~kv_lmdb_() { commit(); close_reader(); }
 
 
-  void close_reader()    { if (r_) mdb_txn_abort(r_),  r_ = nullptr; }
-  void abort()           { if (w_) mdb_txn_abort(w_),  w_ = nullptr; }
+  void close_reader()    { if (r_) mdb_txn_abort(r_), r_ = nullptr; }
+  void abort()           { if (w_) mdb_txn_abort(w_), w_ = nullptr; }
   void sync()   override { close_reader(); commit(); mdb_env_sync(mdb_->e, 1); }
   void commit() override
     { int rc;
@@ -91,9 +91,9 @@ void kv_lmdb_::set(ηic &k, ηic &v)
 {
   MDB_val k_{k.lsize(), (void*) k.ldata()};
   MDB_val v_{v.lsize(), (void*) v.ldata()};
-  int rc;
   while (1)
   {
+    int  rc;
     auto t = w();
     if ((rc = mdb_put(t, dbi_, &k_, &v_, 0)) == MDB_SUCCESS)
       if ((rc = mdb_txn_commit(t)) == MDB_SUCCESS)
@@ -102,9 +102,9 @@ void kv_lmdb_::set(ηic &k, ηic &v)
         return;
       }
 
+    abort();
     if (rc == MDB_MAP_FULL)
     {
-      abort();
       MDB_envinfo i;
       mdb_env_info(mdb_->e, &i);
       mdb_env_set_mapsize(mdb_->e, i.me_mapsize * 2);
